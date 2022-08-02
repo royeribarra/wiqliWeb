f07e935baca754d0bfd64e24845fe7907ef2d05b
royeribarra <royeribarrao@gmail.com>
2022-08-01 20:55:53 -0500

    env y formulario

--- src/pages/datos/datos.js
+++ src/pages/datos/datos.js
@@ -6,6 +6,7 @@ import './datos.css';
 import ProductList from "../../components/productList/productList";
 
 const { Option } = Select;
+const { TextArea } = Input;
 function Datos() 
 {
   const [form] = Form.useForm();
@@ -14,107 +15,91 @@ function Datos()
   }
 
   return (
-      <div className="baseMorada">
-        <Container className="contenedorSimple">
-        <Form 
-                    method="post" 
-                    layout="vertical"
-                    onFinish={onFinish}
-                    form={form}
-                    initialValues={{
-                      departamento: 'Lima',
-                      provincia: 'Lima',
-                      distrito: '',
-                      referencia: ''
-                    }}
-                  >
-                    <div className="row gx-5">
-                      <div className="col-md-6">
-                        <Form.Item
-                          label="Nombres"
-                          name="nombres"
-                          rules={[{ required: true, message: 'Por favor ingresa tu nombre' }]}                
-                        >
-                          <Input className="form-control" placeholder="Nombres"  />
-                        </Form.Item>
-                      </div>
-                      <div className="col-md-6">
-                        <Form.Item
-                          label="Apellidos"
-                          name="apellidos"
-                          rules={[{ required: true, message: 'Por favor ingresa tus apellidos' }]}                
-                        >
-                          <Input className="form-control" placeholder="Apellidos"  />
-                        </Form.Item>
-                      </div>
-                    </div>
-                    <div>
-                      <label>Dirección de recojo</label>
-                    </div>
-                    <div className="row gx-5">
-                      <div className="col-md-6">
-                        <Form.Item
-                          name={"departamento"}
-                          label="Ciudad"
-                        >
-                          <Select>
-                            <Option value="Lima">Lima</Option>
-                          </Select>
-                        </Form.Item>
-                      </div>
-                    </div>
-                    <br />
-                    <Form.Item
-                      name="referencia"
-                      label="Referencia"        
-                    >
-                      <Input className="form-control" placeholder="N° de dpto./of..." />
-                    </Form.Item>
-                    <Form.Item
-                      name="email"
-                      label="Correo"
-                      rules={[{ required: true, message: 'Por favor ingresa tu correo' }]}                
-                    >
-                      <Input className="form-control" placeholder="Correo" />
-                    </Form.Item>
-                    <div className="row gx-5">
-                      <div className="col-md-6">
-                        <Form.Item
-                          name="celular"
-                          label="Celular"
-                          rules={[{ required: true, message: 'Por favor ingresa tu celular' }]}                
-                        >
-                          <Input className="form-control" placeholder="Celular" />
-                        </Form.Item>
-                      </div>
-                      <div className="col-md-6">
-                        <Form.Item
-                          name="dni"
-                          label="DNI"
-                          rules={[{ required: true, message: 'Por favor ingresa tu DNI' }]}                
-                        >
-                          <Input className="form-control" placeholder="DNI" />
-                        </Form.Item>
-                      </div>
-                    </div>
-                    <Form.Item 
-                      name="fecha" 
-                      label="Fecha de recojo" 
-                      rules={[{ required: true, message: 'Selecciona una fecha' }]}
-                      className="selector-fecha"
-                    >
-                      <DatePicker
-                        placeholder="Seleccionar una fecha"
-                        format="YYYY-MM-DD"
-                      />
-                    </Form.Item>
-                    <Button type="primary" htmlType="submit" style={{borderRadius: "28px"}} className="btn btn-repo fw-500 mb-3 mb-sm-0">
-                      Proceder a pagar
-                    </Button>
-                  </Form>
-        </Container>
-      </div>
-
+    <div className="baseMorada">
+      <Container className="contenedorSimple">
+        <Form
+          layout="vertical"
+          onFinish={onFinish}
+          form={form}
+        >
+          <div className="row">
+            <div className="col-md-6">
+              <Form.Item
+                label="Nombres"
+                name="nombres"
+                rules={[{ required: true, message: 'Por favor ingresa tu nombre' }]}                
+              >
+                <Input className="form-control" placeholder="Nombres"  />
+              </Form.Item>
+            </div>
+            <div className="col-md-6">
+              <Form.Item
+                label="Apellidos"
+                name="apellidos"
+                rules={[{ required: true, message: 'Por favor ingresa tus apellidos' }]}                
+              >
+                <Input className="form-control" placeholder="Apellidos"  />
+              </Form.Item>
+            </div>
+            <div className="col-md-6">
+              <Form.Item
+                name="celular"
+                label="Número"
+                rules={[{ required: true, message: 'Por favor ingresa tu celular' }]}                
+              >
+                <Input className="form-control" placeholder="Ej. 939784580" />
+              </Form.Item>
+            </div>
+            <div className="col-md-6">
+              <Form.Item
+                name="email"
+                label="Correo de contacto"
+                rules={[{ required: true, message: 'Por favor ingresa tu correo' }]}                
+              >
+                <Input className="form-control" placeholder="Ej. wiqli@contacto.pe" />
+              </Form.Item>
+            </div>
+            <div className="col-md-6">
+              <Form.Item
+                name="direccion"
+                label="Dirección de recojo"        
+              >
+                <Input className="form-control" placeholder="Av. Aurelio Miroquesada 117" />
+              </Form.Item>
+            </div>
+            <div className="col-md-6">
+              <Form.Item
+                name="referencia"
+                label="Detalle de dirección"        
+              >
+                <Input className="form-control" placeholder="N° de dpto./of..." />
+              </Form.Item>
+            </div>
+            <div className="col-md-6">
+              <Form.Item 
+                name="fecha" 
+                label="Fecha de recojo" 
+                rules={[{ required: true, message: 'Selecciona una fecha' }]}
+                className="selector-fecha"
+              >
+                <DatePicker
+                  placeholder="Seleccionar una fecha"
+                  format="YYYY-MM-DD"
+                />
+              </Form.Item>
+            </div>
+            <div className="col-md-6">
+              <Form.Item label="Anotaciones adicionales">
+                <TextArea rows={4} placeholder="anotaciones adicionales" />
+              </Form.Item>
+            </div>
+          </div>
+          <Button type="primary" htmlType="submit">
+            Proceder a pagar
+          </Button>
+        </Form>
+      </Container>
+    </div>
   );
 }
   
