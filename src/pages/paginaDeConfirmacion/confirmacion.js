82ce85c2b1e78a5e909f94add9ce05d7c3c0618d
royeribarra <royeribarrao@gmail.com>
2022-08-10 20:59:25 -0500

    corrigiendo la ruptura de pagina

--- src/pages/paginaDeConfirmacion/confirmacion.js
+++ src/pages/paginaDeConfirmacion/confirmacion.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from "react-bootstrap";
 import './confirmacion.css';
 import logo from "../../images/logo.png"
