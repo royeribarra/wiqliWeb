d4b761cb311e7b3bc7479a0ecbf6e136b855ec83
Renzo Anaya <r.anayaa@alum.up.edu.pe>
2022-11-24 10:46:07 -0500

    registro completo

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,6 +3,9 @@ import { Container } from "react-bootstrap";
 import { Form, Input, Button, Spin } from 'antd';
 import './login.css';
 import logo from "../../images/logo.png"
+import facebook from "../../images/logoFacebook.png";
+import google from "../../images/logoGmail.png";
+import apple from "../../images/logoApple.png";
 
 
 function Login() 
@@ -10,35 +13,52 @@ function Login()
   return (
     <div className="gradienteMedio">
     
-        <Container className="contenedorSimple">
+        <Container className="contenedorDeForm contenedorSimple">
         <div>
-        <h2 className="tituloPrincipal">Inicia Sesión</h2>
-        <Form>
-        <div className="itemForm">
-                <Form.Item
-                  name="correo"
-                  label="Correo electrónico"
-                  rules={[{ required: true, message: 'Por favor ingresa tu correo' }]}                
-                >
-                  <Input className="form-control" placeholder="Ej. nombre@mail.com" />
+            <h2 className="tituloPrincipal">Inicia Sesión</h2>
+            <div>
+                <Form>
+                <div className="itemForm">
+                        <Form.Item
+                        name="correo"
+                        label="Correo electrónico"
+                        rules={[{ required: true, message: 'Por favor ingresa tu correo' }]}                
+                        >
+                        <Input className="form-control" placeholder="Ej. nombre@mail.com" />
+                        </Form.Item>
+                </div>
+                <div className="itemForm">
+                        <Form.Item
+                        name="password"
+                        label="Contraseña"
+                        rules={[{ required: true, message: 'Por favor ingresa tu contraseña' }]}                
+                        >
+                        <Input className="form-control" placeholder="**********" />
+                        </Form.Item>
+                </div>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="botonFinal botonLogin">
+                        Iniciar sesión
+                    </Button>
                 </Form.Item>
-        </div>
-        <div className="itemForm">
-                <Form.Item
-                  name="password"
-                  label="Contraseña"
-                  rules={[{ required: true, message: 'Por favor ingresa tu contraseña' }]}                
-                >
-                  <Input className="form-control" placeholder="**********" />
-                </Form.Item>
-        </div>
-        <Form.Item>
-            <Button type="primary" htmlType="submit" className="botonFinal botonLogin">
-                Enviar pedido
-            </Button>
-        </Form.Item>
+                </Form>
+            </div>
+            <p className='signoO'>ó</p>
+            <div className='opcionesDeSesion'>
+                <div className='cardOpcionDeSesion'>
+                    <img className='logoSesion' src={google}></img>
+                    <p>Inicia sesión con Google</p>
+                </div>
+                <div className='cardOpcionDeSesion'>
+                    <img className='logoSesion' src={apple}></img>
+                    <p>Inicia sesión con Apple</p>
+                </div>
+                <div className='cardOpcionDeSesion'>
+                    <img className='logoSesion' src={facebook}></img>
+                    <p>Inicia sesión con Facebook</p>
+                </div>
 
-        </Form>
+            </div>
         </div>
 
         </Container>
