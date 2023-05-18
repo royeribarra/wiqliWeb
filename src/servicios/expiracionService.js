8e741fb32af9553064c4d0d505574d2a4cadf0ca
royeribarra <royeribarrao@gmail.com>
2023-05-17 20:27:41 -0500

    quitando navigate del servicio

--- src/servicios/expiracionService.js
+++ src/servicios/expiracionService.js
@@ -1,22 +1,21 @@
-import { useNavigate } from "react-router-dom";
 import { fillCart } from "../redux/actions/carritoActions";
 
 export class ExpiracionService
 {
     comprobarExpiracion() {
-        
-        const expiration = localStorage.getItem("expiration");
-        if(expiration)
-        {
-            const history = useNavigate();
-            const exp2 = new Date(expiration)
-            const now = new Date();
-            if ((now.getTime() - exp2.getTime()) > (12 * 60 * 60 * 1000)) 
-            { 
-                localStorage.removeItem("expiration");
-                dispatch(fillCart([]));
-                history(`/`);
-            }
-        }
+        console.log("hola");
+        // const expiration = localStorage.getItem("expiration");
+        // if(expiration)
+        // {
+        //     const history = useNavigate();
+        //     const exp2 = new Date(expiration)
+        //     const now = new Date();
+        //     if ((now.getTime() - exp2.getTime()) > (12 * 60 * 60 * 1000)) 
+        //     { 
+        //         localStorage.removeItem("expiration");
+        //         dispatch(fillCart([]));
+        //         history(`/`);
+        //     }
+        // }
     }
 }
